e1e9e083060b34951c66da9777569faad44f6e88
Chris Hatch <hatch@tuta.io>
2023-08-07 06:34:47 +0700

    update grpc client to use the new EmliteResponse

--- emlite/grpc/emlite-mediator-client.py
+++ emlite/grpc/emlite-mediator-client.py
@@ -1,9 +1,13 @@
+import datetime
 import logging
 import grpc
 import sys
 
+from kaitaistruct import KaitaiStream, BytesIO
+
 from .generated.emlite_mediator_pb2 import ObjectId, ReadElementRequest, SendRawMessageRequest
 from .generated.emlite_mediator_pb2_grpc import EmliteMediatorServiceStub
+from ..messages.emlite_response import EmliteResponse
 
 FORMAT = '%(asctime)s %(levelname)s %(module)s %(message)s'
 logging.basicConfig(format=FORMAT, level=logging.INFO)
@@ -12,30 +16,40 @@ logger = logging.getLogger(__name__)
 def send_message(stub, message):
     try:
         rsp = stub.sendRawMessage(SendRawMessageRequest(dataField=message))
-        logger.info('got serial %s', rsp.response.decode('ascii'))   
     except grpc.RpcError as e:
         logger.error('sendRawMessage failed: [%s] (code: %s)', e.details(), e.code())
+    return rsp
 
 def read_element(stub, object_id):
     try:
         rsp = stub.readElement(ReadElementRequest(objectId=object_id))
     except grpc.RpcError as e:
         logger.error('readElement failed: [%s] (code: %s)', e.details(), e.code())
-
-    if (object_id == ObjectId.SERIAL):
-        logger.info('got serial %s', rsp.response.decode('ascii'))   
-    else:
-        logger.info('got response %s', rsp.response.hex())   
-
+    return rsp
+    
 def run(mediator_address):
     with grpc.insecure_channel(mediator_address) as channel:
         stub = EmliteMediatorServiceStub(channel)
 
-        # message = bytes.fromhex('0160010000')
-        # send_message(stub, message)
+        # get serial by raw message:
+        # rsp = send_message(stub, bytes.fromhex('0160010000'))
 
-        # read_element(stub, ObjectId.SERIAL)
-        read_element(stub, ObjectId.TIME)
+        object_id_grpc = ObjectId.TIME
+        payload_bytes = read_element(stub, object_id_grpc).response
+        logger.info('payload_bytes: [%s]', payload_bytes.hex())
+
+        object_id = EmliteResponse.ObjectIdType.time
+        emlite_rsp = EmliteResponse(len(payload_bytes), object_id, KaitaiStream(BytesIO(payload_bytes)))
+        emlite_rsp._read()
+
+        data = emlite_rsp.response
+        if (object_id == EmliteResponse.ObjectIdType.serial):
+            logger.info('serial %s', data.serial.strip())   
+        elif (object_id == EmliteResponse.ObjectIdType.time):
+            date_obj = datetime.datetime(2000 + data.year, data.month, data.date, data.hour, data.minute, data.second)
+            logger.info('time %s', date_obj.isoformat())   
+        else:
+            logger.info('response %s', payload_bytes.response.hex())   
 
 if __name__ == '__main__':
     if (len(sys.argv) == 1):
