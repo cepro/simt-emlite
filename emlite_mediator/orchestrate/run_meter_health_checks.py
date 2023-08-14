d58b780a270c71094131082355f6b658d1df29ee
Chris Hatch <hatch@tuta.io>
2023-08-14 22:56:58 +0700

    update run_meter_health_checks to run only 5 checks at a time so the mediators don't fill up memory

--- emlite_mediator/orchestrate/run_meter_health_checks.py
+++ emlite_mediator/orchestrate/run_meter_health_checks.py
@@ -32,48 +32,60 @@ class RunHealthChecks():
         logger.info("%s starting ...", __name__)
 
         registry_result = self.supabase.table(
-            'meter_registry').select('id,ip_address,serial').execute()
+            'meter_registry').select('id,ip_address,serial').order(column='serial').execute()
         if (len(registry_result.data) == 0):
             logger.error("no meters record found")
             sys.exit(11)
 
         meters = list(filter(filter_connected, registry_result.data))
 
-        for meter in meters:
-            logger.info(
-                "sync meter [serial=%s, ip=%s]",
-                meter['serial'] if 'serial' in meter else 'unknown',
-                meter['ip_address']
-            )
+        # process meter health checks in chunks of chunk_size to avoid
+        # running out of memory from starting mediators
+        chunk_size = 5
+        num_chunks = (len(meters) // chunk_size) + 1
+        for chunk_idx in list(range(0, num_chunks)):
+            range_start = chunk_idx * chunk_size
+            range_end = range_start + chunk_size
+            chunk_meters = meters[range_start:range_end]
 
-            mediator_port = self.mediators.start_one(meter['id'])
+            for meter in chunk_meters:
+                logger.info(
+                    "meter [serial=%s, ip=%s]",
+                    meter['serial'] if 'serial' in meter else 'unknown',
+                    meter['ip_address']
+                )
 
-            env_vars = {
-                "METER_ID": meter['id'],
-                "MEDIATOR_PORT": mediator_port,
-                "SUPABASE_URL": supabase_url,
-                "SUPABASE_KEY": supabase_key
-            }
+                mediator_port = self.mediators.start_one(meter['id'])
 
-            logger.info('health_check meter=%s mediator port=%s',
-                        meter['id'], mediator_port)
+                env_vars = {
+                    "METER_ID": meter['id'],
+                    "MEDIATOR_PORT": mediator_port,
+                    "SUPABASE_URL": supabase_url,
+                    "SUPABASE_KEY": supabase_key
+                }
 
-            self.docker_client.containers.run(
-                mediator_image,
-                name=f"meter-health-check-{meter['ip_address']}",
-                command="emlite_mediator.jobs.meter_health_check",
-                environment=env_vars,
-                network_mode="host",
-                detach=True,
-                remove=True,
-            )
+                logger.info('health_check meter=%s mediator port=%s',
+                            meter['id'], mediator_port)
 
-            # Slow down invocation a litte just so we don't fire off all at the same time.
-            # Each takes less than 10 seconds to run so this sleep results in 3 or 4 running
-            # in parallel at a time.
-            time.sleep(3)
+                self.docker_client.containers.run(
+                    mediator_image,
+                    name=f"meter-health-check-{meter['ip_address']}",
+                    command="emlite_mediator.jobs.meter_health_check",
+                    environment=env_vars,
+                    network_mode="host",
+                    detach=True,
+                    remove=True,
+                )
 
-        self._stop_mediators(list(map(lambda meter: meter['id'], meters)))
+                # Slow down invocation a litte just so we don't fire off all at the same time.
+                # Each takes less than 10 seconds to run so this sleep results in 3 or 4 running
+                # in parallel at a time.
+
+                # DISABLED whilst we have chunking in place:
+                # time.sleep(3)
+
+            self._stop_mediators(
+                list(map(lambda meter: meter['id'], chunk_meters)))
 
     def _stop_mediators(self, meter_ids: List[str]):
         for meter_id in meter_ids:
