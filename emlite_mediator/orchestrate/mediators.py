ce03bc124610a4f4c826fa1f59436659c816e6d9
Chris Hatch <hatch@tuta.io>
2023-08-16 18:02:14 +0700

    remove concept of always up list and just assume ALL mediators will start and are always up added stop_all and start_all

--- emlite_mediator/orchestrate/mediators.py
+++ emlite_mediator/orchestrate/mediators.py
@@ -13,17 +13,6 @@ from emlite_mediator.util.logging import get_logger
 
 logger = get_logger('mediators')
 
-# Initially this handful of meters are the only ones running a mediator all
-# the time. The following are the emnify id's of all the WLCE devices
-# tagged 'Landlord'.
-# Later all meters will run a mediator all the time
-ALWAYS_UP_EMNIFY_IDS = [
-    12585053,  # Plot 18-19.C
-    12108323,  # Plot 18-19.G (3 phase)
-    9651015,   # Plot 24-25.C
-    12585039,  # Plot 24-25.G (3 phase)
-]
-
 mediator_image: str = os.environ.get("MEDIATOR_IMAGE")
 supabase_url: str = os.environ.get("SUPABASE_URL")
 supabase_key: str = os.environ.get("SUPABASE_KEY")
@@ -37,7 +26,7 @@ class Mediators():
         self.supabase = create_client(supabase_url, supabase_key)
         self.docker_client = docker.from_env()
 
-    def start_one(self, meter_id: str, always_up=False) -> int:
+    def start_one(self, meter_id: str) -> int:
         container = self._container_by_meter_id(meter_id)
         if (container is not None):
             logger.info('start existing container [%s]', container.name)
@@ -49,10 +38,6 @@ class Mediators():
         logger.info("create mediator container [listen=%s, ip=%s, id=%s]",
                     mediator_port, ip_address, meter_id)
 
-        restart_policy = None
-        if always_up == True:
-            restart_policy = {"Name": "always"}
-
         self.docker_client.containers.run(
             mediator_image,
             name=f"mediator-{ip_address}",
@@ -62,9 +47,8 @@ class Mediators():
                 "LISTEN_PORT": mediator_port
             },
             network_mode="host",
-            restart_policy=restart_policy,
+            restart_policy={"Name": "always"},
             labels={
-                "always_up": str(always_up),
                 "meter_id": meter_id,
                 "emlite_host": ip_address,
                 "listen_port": str(mediator_port)
@@ -74,33 +58,33 @@ class Mediators():
 
         return mediator_port
 
-    def start_many(self, meter_ids: List[str], always_up=False) -> Dict[str, int]:
+    def start_many(self, meter_ids: List[str]) -> Dict[str, int]:
         logger.info('start_many: ids [%s]', meter_ids)
         ports = {}
         for meter_id in meter_ids:
-            ports['meter_id'] = self.start_one(meter_id, always_up)
+            ports['meter_id'] = self.start_one(meter_id)
         return ports
 
-    def start_always_up_set(self, emnify_ids: List[str]) -> Dict[str, int]:
+    def start_all(self) -> Dict[str, int]:
         try:
             rows = self.supabase.table('meter_registry').select(
-                'id').in_("emnify_id", emnify_ids).execute()
+                'id').execute()
         except ConnectError as e:
             logger.error("Supabase connection failure [%s]", e)
             sys.exit(10)
         ids = list(map(lambda row: row['id'], rows.data))
-        return self.start_many(ids, always_up=True)
+        return self.start_many(ids)
+
+    def stop_all(self):
+        mediator_containers = self._all_running_mediators()
+        for mediator in mediator_containers:
+            mediator.stop()
 
     def stop_one(self, meter_id: str):
         container = self._container_by_meter_id(meter_id)
         if (container == None):
             logger.info(
                 "stop_one: skip - container for meter [%s] already stopped", meter_id)
-            return
-
-        if (container.labels['always_up'] == "True"):
-            logger.info(
-                "stop_one: skip - container has always_up set to True for meter [%s]", meter_id)
             return
 
         logger.info(
@@ -113,6 +97,11 @@ class Mediators():
         if (len(containers) == 0):
             return None
         return containers[0]
+
+    def _all_running_mediators(self):
+        containers = self.docker_client.containers.list(all=True,
+                                                        filters={"status": "running"})
+        return list(filter(lambda c: c.name.startswith('mediator-'), containers))
 
     def _get_meter_ip(self, meter_id: str):
         meter_registry_record = None
@@ -152,4 +141,4 @@ if __name__ == '__main__':
         exit(2)
 
     mediators = Mediators()
-    mediators.start_always_up_set(ALWAYS_UP_EMNIFY_IDS)
+    mediators.start_all()
