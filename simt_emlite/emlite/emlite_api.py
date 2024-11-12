2b55fc7ec98cbdcb73909339a219b9b98e312460
Chris Hatch <hatch@tuta.io>
2024-11-12 17:43:52 +0700

    handle responses in event log format as default format

--- simt_emlite/emlite/emlite_api.py
+++ simt_emlite/emlite/emlite_api.py
@@ -117,7 +117,10 @@ class EmliteAPI:
         # which is 12 bytes (length 1, control 1, destination 4, source 4, crc 2)
         #   plus data field length which depends on format and payload
         frame_length = 12
-        if data_field.format == EmopData.RecordFormat.default:
+        if data_field.format in [
+            EmopData.RecordFormat.default,
+            EmopData.RecordFormat.event_log,
+        ]:
             # 5 (format 1, object id 3, rw flag 1) plus payload length
             frame_length = frame_length + 5 + len(data_field.message.payload)
         else:
