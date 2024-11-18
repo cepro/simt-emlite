184ce8ab5892fac56d383a3cf45360b1e879c185
Chris Hatch <hatch@tuta.io>
2024-11-18 10:53:28 +0700

    add basic test for filter_event_log_for_unseen_events and rename the test module

--- tests/simt_emlite/sync/test_syncer_event_log_unseen_events_filter.py
+++ tests/simt_emlite/sync/test_syncer_event_log_unseen_events_filter.py
@@ -12,9 +12,38 @@ EventRec = EmopMessage.EventRec
 
 ONE_DAY_SECONDS = 1440
 
+START_TIME_SECONDS = 784598400  # 11/11/2024 0:00 in seconds since 01/01/2000
+
 
 def add_days(ts: int, days: int) -> int:
     return ts + (days * ONE_DAY_SECONDS)
+
+
+def event(ts: int, id: EventIdType, set: int) -> EventRec:
+    event = EventRec()
+    event.timestamp = ts
+    event.event_id = id
+    event.event_set = set
+    return event
+
+
+EVENTS: List[EventRec] = [
+    event(add_days(START_TIME_SECONDS, 1), EventIdType.daily_log_snapshot, set=4),
+    event(add_days(START_TIME_SECONDS, 1), 52, set=0),
+    event(add_days(START_TIME_SECONDS, 1), EventIdType.billing_log_snapshot, set=6),
+    event(
+        # 20:00 on the 11/11/2024
+        START_TIME_SECONDS + 20 * 60 * 60,
+        EventIdType.listening_socket_aborted,
+        set=0,
+    ),
+    event(add_days(START_TIME_SECONDS, 1), EventIdType.daily_log_snapshot, set=5),
+    event(add_days(START_TIME_SECONDS, 1), 52, set=0),
+    event(add_days(START_TIME_SECONDS, 1), EventIdType.billing_log_snapshot, set=7),
+    event(add_days(START_TIME_SECONDS, 2), EventIdType.daily_log_snapshot, set=6),
+    event(add_days(START_TIME_SECONDS, 2), 52, set=0),
+    event(add_days(START_TIME_SECONDS, 2), EventIdType.billing_log_snapshot, set=8),
+]
 
 
 class TestSyncerEventLogUnseenEventsFilter(unittest.TestCase):
@@ -25,23 +54,8 @@ class TestSyncerEventLogUnseenEventsFilter(unittest.TestCase):
         )
 
     def test_one_new_event(self):
-        time_seconds = 784598400  # 11/11/2024 0:00
+        last_seen_event = EVENTS[8]
 
-        events: List[EventRec] = [
-            self._event(time_seconds, EventIdType.daily_log_snapshot, 5),
-            self._event(time_seconds, EventIdType.billing_log_snapshot, 3),
-            self._event(add_days(time_seconds, 1), EventIdType.daily_log_snapshot, 6),
-        ]
-
-        last_seen_event = events[1]
-
-        unseen = filter_event_log_for_unseen_events(events, last_seen_event)
+        unseen = filter_event_log_for_unseen_events(EVENTS, last_seen_event)
         self.assertEqual(1, len(unseen))
-        self.assertEqual(events[2], unseen[0])  # identity same
-
-    def _event(self, ts: int, id: EventIdType, seq: int) -> EventRec:
-        event = EventRec()
-        event.timestamp = ts
-        event.event_id = id
-        event.event_seq = seq
-        return event
+        self.assertEqual(EVENTS[9], unseen[0])  # identity same
