25f7ffab3a391a71575502906b60361ccb7dcf0c
Chris Hatch <hatch@tuta.io>
2024-12-18 09:24:00 +1000

    load env variables inside factory.get_instance as they are not loaded at import time

--- simt_emlite/orchestrate/adapter/factory.py
+++ simt_emlite/orchestrate/adapter/factory.py
@@ -3,12 +3,17 @@ import os
 from simt_emlite.orchestrate.adapter.docker_adapter import DockerAdapter
 from simt_emlite.orchestrate.adapter.fly_adapter import FlyAdapter
 
-fly_api_token = os.environ.get("FLY_API_TOKEN")
-fly_dns_server = os.environ.get("FLY_DNS_SERVER") or "fdaa::3"
-mediator_image = os.environ.get("SIMT_EMLITE_IMAGE")
-
 
 def get_instance(esco: str = None):
+    # ideally load these outside the function however due to the way we load
+    # the env variables in the cli tool (in the body and not before the script
+    # is invoked) this module gets executed before env loading as imports are
+    # above. better would be a wrapper around the cli that sets up all env
+    # variables first so consider moving to that.
+    fly_api_token = os.environ.get("FLY_API_TOKEN")
+    fly_dns_server = os.environ.get("FLY_DNS_SERVER") or "fdaa::3"
+    mediator_image = os.environ.get("SIMT_EMLITE_IMAGE")
+
     if fly_api_token is not None:
         adapter = FlyAdapter(fly_api_token, fly_dns_server, mediator_image, esco)
     else:
