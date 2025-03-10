5798f7b3581ca9f178766126c35a76f3fc637e72
Chris Hatch <devcebfaff@proton.me>
2025-03-10 10:25:26 +1000

    skip three phase meters when syncing load_switch as they don't support this obid

--- simt_emlite/sync/syncer_load_switch.py
+++ simt_emlite/sync/syncer_load_switch.py
@@ -3,6 +3,7 @@ from typing_extensions import override
 
 from simt_emlite.sync.syncer_base import SyncerBase, UpdatesTuple
 from simt_emlite.util.logging import get_logger
+from simt_emlite.util.meters import is_three_phase_lookup
 
 logger = get_logger(__name__, __file__)
 
@@ -10,6 +11,10 @@ logger = get_logger(__name__, __file__)
 class SyncerLoadSwitch(SyncerBase):
     @override
     def fetch_metrics(self) -> UpdatesTuple:
+        is_3p = is_three_phase_lookup(self.supabase, self.meter_id)
+        if is_3p:
+            return
+
         setting = self.emlite_client.load_switch()
 
         if not isinstance(setting, EmopMessage.LoadSwitchSettingType):
