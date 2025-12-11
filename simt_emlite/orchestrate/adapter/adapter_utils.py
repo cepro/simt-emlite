10b97e3ad9c7fdb9ae9e06659f48a323f1c2bfd9
Chris Hatch <devcebfaff@proton.me>
2025-12-12 00:17:51 +1000

    add logging to build_app_name

--- simt_emlite/orchestrate/adapter/adapter_utils.py
+++ simt_emlite/orchestrate/adapter/adapter_utils.py
@@ -1,9 +1,13 @@
 import os
 
+from simt_emlite.util.logging import get_logger
+
+logger = get_logger(__name__, __file__)
 
 def build_app_name(
     is_single_meter_app: bool, serial: str | None, esco: str | None, env: str | None
 ) -> str:
+    logger.info(f"Building app name for single meter app: {is_single_meter_app}, serial: {serial}, esco: {esco}, env: {env}")
     use_env = True if env is not None and (env != "prod" and env != "qa") else False
     env_part = f"-{env}" if use_env else ""
 
@@ -12,9 +16,11 @@ def build_app_name(
     elif env is not None:
         app_name = f"mediators{env_part}-{esco}"
     else:
+        logger.info("Using FLY_APP_NAME")
         fly_app_name = os.environ.get("FLY_APP_NAME")
         if fly_app_name is None:
             raise Exception("One of ENV or FLY_APP_NAME must be set")
         app_name = fly_app_name
 
+    logger.info(f"Built app name: {app_name}")
     return app_name.lower()
