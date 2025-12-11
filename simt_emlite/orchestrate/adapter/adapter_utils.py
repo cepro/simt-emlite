a5789b222c8cb97a7d4894fa0c7485363442998a
Chris Hatch <devcebfaff@proton.me>
2025-12-11 13:18:33 +1000

    check FLY_APP_NAME set

--- simt_emlite/orchestrate/adapter/adapter_utils.py
+++ simt_emlite/orchestrate/adapter/adapter_utils.py
@@ -1,12 +1,20 @@
-def build_app_name(is_single_meter_app: bool, serial: str | None, esco: str | None, env: str | None) -> str:
+import os
+
+
+def build_app_name(
+    is_single_meter_app: bool, serial: str | None, esco: str | None, env: str | None
+) -> str:
     use_env = True if env is not None and (env != "prod" and env != "qa") else False
-    env_part = f"-{env}" if use_env else None
+    env_part = f"-{env}" if use_env else ""
 
     if is_single_meter_app:
         app_name = f"mediator{env_part}-{serial}"
-    elif env_part is not None:
+    elif env is not None:
         app_name = f"mediators{env_part}-{esco}"
     else:
-        app_name = os.environ.get("FLY_APP_NAME")
+        fly_app_name = os.environ.get("FLY_APP_NAME")
+        if fly_app_name is None:
+            raise Exception("One of ENV or FLY_APP_NAME must be set")
+        app_name = fly_app_name
 
     return app_name.lower()
