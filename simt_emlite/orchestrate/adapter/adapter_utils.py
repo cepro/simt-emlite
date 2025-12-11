7c75dbb4af1630c8290f35220faaec566ff866c9
Chris Hatch <devcebfaff@proton.me>
2025-12-11 13:15:07 +1000

    patch container instance factory: fall back to FLY_APP_NAME if not all variable provided

--- simt_emlite/orchestrate/adapter/adapter_utils.py
+++ simt_emlite/orchestrate/adapter/adapter_utils.py
@@ -1,10 +1,12 @@
 def build_app_name(is_single_meter_app: bool, serial: str | None, esco: str | None, env: str | None) -> str:
     use_env = True if env is not None and (env != "prod" and env != "qa") else False
-    env_part = f"-{env}" if use_env else ""
+    env_part = f"-{env}" if use_env else None
 
     if is_single_meter_app:
         app_name = f"mediator{env_part}-{serial}"
-    else:
+    elif env_part is not None:
         app_name = f"mediators{env_part}-{esco}"
+    else:
+        app_name = os.environ.get("FLY_APP_NAME")
 
     return app_name.lower()
